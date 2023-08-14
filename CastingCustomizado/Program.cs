e055deae39e2973186114633c1f5dfbea789b9cb
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-08-14 13:20:17 -0300

    casting customizados

--- CastingCustomizado/Program.cs
+++ CastingCustomizado/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        LetraAlfabeto la = new LetraAlfabeto("b");
+        char c = la
+    }
+}
+
+class LetraAlfabeto
+{
+    char caractere;
+
+    public Program(char caractere)
+    {
+        this.caractere = char.ToUpper(caractere);
+    }
+
+    public static implicit operator char(LetraAlfabeto la)
+    {
+        return la.caractere;
+    }
+
+       public static implicit operator LetraAlfabeto(char c)
+    {
+        return new LetraAlfabeto(c);
+    }
+}
