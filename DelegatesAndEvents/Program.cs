de671ab83452739879f3ed6b8269bb81852e43c2
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-08-21 19:44:19 -0300

    Delegates 1

--- DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Threading;
+using System.Reflection.PortableExecutable;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Semaphore s = new Semaphore();
+        s.Init();
+    }
+
+}
+
+enum Color {
+    Red,
+    Green,
+    Yellow
+}
+
+class Semaphore
+{
+    Color color = Color.Red;
+    public void Init()
+    {
+        while (true)
+        {
+            if (color == Color.Red) color = Color.Green;
+            else if (color == Color.Green) color = Color.Yellow;
+            else if (color == Color.Yellow) color = Color.Red;
+
+            System.Console.WriteLine($"Change to: {color}");
+
+            Thread.Sleep(2000);
+        }
+    }
+}
