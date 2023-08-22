c999f0b00b4d32e556333df0d7de84c0466dc37b
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-08-21 21:19:55 -0300

    fazendo o 2.3

--- DelegatesGenericos/Program.cs
+++ DelegatesGenericos/Program.cs
@@ -4,6 +4,8 @@ class Program
     public static void Main(string[] args)
     {
         
+
+
         Temperature t= new Temperature();
         
         Console.WriteLine("Insira a medida: ");
@@ -12,9 +14,17 @@ class Program
         Console.WriteLine("Insira o valor: ");
         var value = Double.Parse(Console.ReadLine());
         
-        TempConverter convertToC = t.ToCelcius;
-        TempConverter convertToF = t.ToFiren;
-    
+        //TempConverter convertToC = t.ToCelcius;
+        //TempConverter convertToF = t.ToFiren;
+
+        // Aceita metodos void
+        Action<double> AconvertToF = t.PrintToFiren;
+        Action<double> AconvertToC = t.PrintToCelcius;
+
+        // Não aceita void
+        Func<double, double> convertToF = t.ToFiren;
+        Func<double, double> convertToC = t.ToCelcius;
+        
         if (tempMetric == "F") Console.WriteLine(convertToF(value));
         if (tempMetric == "C") Console.WriteLine(convertToC(value));
         else Console.WriteLine("Não Encontrado!");
@@ -36,5 +46,14 @@ class Temperature
     public double ToCelcius(double firen)
     {
         return (firen  - 32) * 5/9;
+    }
+
+    public void PrintToCelcius(double firen)
+    {
+        Console.WriteLine($"O valor em Firenheit é: {ToCelcius(firen).ToString("F2")}");
+    }
+    public void PrintToFiren(double celcius)
+    {
+        Console.WriteLine($"O valor em Celcius é: {ToFiren(celcius).ToString("F2")}");
     }
 }
