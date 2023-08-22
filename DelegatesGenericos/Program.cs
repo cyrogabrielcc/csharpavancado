d751528920d39fb476541f344cfd273af00e41a4
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-08-21 21:00:44 -0300

    fazendo o 2.3

--- DelegatesGenericos/Program.cs
+++ DelegatesGenericos/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        Temperature t= new Temperature();
+        
+        Console.WriteLine("Insira a medida: ");
+        var tempMetric = Console.ReadLine();
+        
+        Console.WriteLine("Insira o valor: ");
+        var value = Double.Parse(Console.ReadLine());
+        
+        TempConverter convertToC = t.ToCelcius;
+        TempConverter convertToF = t.ToFiren;
+    
+        if (tempMetric == "F") Console.WriteLine(convertToF(value));
+        if (tempMetric == "C") Console.WriteLine(convertToC(value));
+        else Console.WriteLine("Não Encontrado!");
+        
+            
+        
+    }
+}
+
+public delegate double TempConverter(double temp);
+
+class Temperature
+{
+    public double ToFiren(double celcius)
+    {
+       return (celcius * 9 / 5) + 32;
+    }
+
+    public double ToCelcius(double firen)
+    {
+        return (firen  - 32) * 5/9;
+    }
+}
