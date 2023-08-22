3e87173db5ace34f16ab2719620cf683af3857d2
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-08-21 21:44:40 -0300

    Eventos Iniciando

--- Events/Program.cs
+++ Events/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        NumberGenerator numberGenerator= new NumberGenerator();
+        numberGenerator.OneGenerated += NumberGenerated;
+        numberGenerator.Start();
+    }
+
+    static void NumberGenerated(int n) => Console.WriteLine($"O numero gerado é {n}");
+}
+
+public delegate void NumberHandler(int i);
+
+class NumberGenerator
+{
+    public event NumberHandler OneGenerated;
+    Random random = new Random();
+
+    public void Start()
+    {
+        while (true)
+        {
+            var n = random.Next(100);
+
+            if(OneGenerated != null)  OneGenerated(n) ;
+
+            Thread.Sleep(600);
+        }
+    }
+}
