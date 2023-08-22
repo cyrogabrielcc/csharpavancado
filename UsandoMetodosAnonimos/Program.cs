1f462593f353001c6de19d7df36e0f9f915b3422
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-08-22 17:22:07 -0300

    aula 3.2

--- UsandoMetodosAnonimos/Program.cs
+++ UsandoMetodosAnonimos/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.VisualBasic;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        NumberGenerator numberGenerator= new NumberGenerator();
+        numberGenerator.OneGenerated += delegate(object sender, NumberEventsArgs args) 
+    { 
+        Console.WriteLine($"O numero gerado é {args.Number}");
+    };
+        numberGenerator.Start();
+    }
+
+}
+
+public delegate void NumberHandler(object sender, NumberEventsArgs args);
+
+class NumberGenerator
+{
+    public event NumberHandler OneGenerated;
+    Random random = new Random();
+
+    public void Start()
+    {
+        while (true)
+        {
+            var n = random.Next(10);
+
+            if(OneGenerated != null)
+            {
+                NumberEventsArgs args = new NumberEventsArgs(){Number = n};
+                OneGenerated(this, args);
+            }
+
+            Thread.Sleep(600);
+        }
+    }
+}
+
+public class NumberEventsArgs : EventArgs
+{
+    public int Number { get; set; }
+}
+
